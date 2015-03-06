bbd0c362d5baadb6a1cac35d9387e31a442fb4ff
zfz <zhang.fangzhou@immomo.com>
2015-03-06 11:41:52 +0800

    Implemented Regularized Linear Regression Cost Function and Regression Gradient of exercise 5

--- mlclass-ex5-008/mlclass-ex5/linearRegCostFunction.m
+++ mlclass-ex5-008/mlclass-ex5/linearRegCostFunction.m
@@ -19,14 +19,11 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+H = X * theta;
+J = sum((H - y) .** 2) / (2*m) + lambda/(2*m) * sum(theta(2:end) .** 2);
 
-
-
-
-
-
-
-
+grad = X' * (H - y) / m;
+grad(2:end) = grad(2:end) + lambda/m * theta(2:end);
 
 
 
